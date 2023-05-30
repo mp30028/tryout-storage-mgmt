4e22d83e019244d187e0589563ee29abae170f46
mp30028 <mp30028@gmail.com>
2023-05-30 08:00:49 +0100

    added shell scripts to tryout sops and convenience script

---
+++ sops-setup/try-it-out/try-it-2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "key1: Value_One" >  config.yaml
+echo "key2: Value_Two" >> config.yaml
+echo "key3: Value_Three" >> config.yaml
+
+echo "----------------------------------------------------------------------"
+echo "    Test input file, config.yaml, created with the following content"
+echo "----------------------------------------------------------------------"
+cat config.yaml
+echo "======================================================================"
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo "----------------------------------------------------------------------"
+echo "               Encrypting all keys in config.yaml"
+echo "----------------------------------------------------------------------"
+echo ""
+echo ""
+../encrypt-with-sops.sh config.yaml "key1|key3"
+echo ""
+echo ""
+cat config.yaml
+echo ""
+echo ""
+echo "======================================================================"
+
+echo ""
+echo ""
+echo "run \"sops config.yaml\" to view and edit an unencrypted version of the"
+echo "file and to re-encrypt any changes automatically"
